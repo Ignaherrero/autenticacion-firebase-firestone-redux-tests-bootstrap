b3c370bba244ef27ec6823e0a1b39e101bf73321
Your Name <nachoherrero93@hotmail.com>
2021-07-05 19:22:38 -0300

    elimino console.log

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,7 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(process.env);
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
